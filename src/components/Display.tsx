054ca45d61e454ecbf7a59ded3684e576398c8da
yu <38282449+yyykt@users.noreply.github.com>
2020-05-04 01:13:48 +0900

    temporary style for display and button

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,23 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  margin: 0 0 20px 0;
+  padding: 20px;
+  border: 4px solid #333;
+  border-radius: 20px;
+  background: #fff;
+  width: 100%;
+  font-size: 0.8em;
+`;
 interface Props {
   gameOver?: boolean;
   text: string;
 }
 
-const Display: FC<Props> = ({ gameOver, text }) => <div>{text}</div>;
+const Display: FC<Props> = ({ gameOver, text }) => <Wrapper>{text}</Wrapper>;
 
 export default Display;
--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,8 +1,19 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.button`
+  box-sizing: border-box;
+  background: #fff;
+  min-height: 40px;
+  width: 100%;
+  cursor: pointer;
+`;
 
 interface Props {
   callback?: any;
 }
-const StartButton: FC<Props> = ({ callback }) => <div>Start Game</div>;
+const StartButton: FC<Props> = ({ callback }) => (
+  <Wrapper onClick={callback}>Start Game</Wrapper>
+);
 
 export default StartButton;

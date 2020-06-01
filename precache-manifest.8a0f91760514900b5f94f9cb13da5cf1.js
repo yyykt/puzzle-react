c8c439431ac1f46041e0bcb40ab06aa489209d2e
yu <38282449+yyykt@users.noreply.github.com>
2020-06-01 10:48:41 +0900

    Updates

---
+++ precache-manifest.8a0f91760514900b5f94f9cb13da5cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5721584d00ed379f111a09c8e4f96de",
+    "url": "/puzzle-react/index.html"
+  },
+  {
+    "revision": "8dfaf3f6d5ce54dc6f74",
+    "url": "/puzzle-react/static/css/main.aa59747b.chunk.css"
+  },
+  {
+    "revision": "aa36a20a1273fdcd4ce5",
+    "url": "/puzzle-react/static/js/2.98fffa6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/puzzle-react/static/js/2.98fffa6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfaf3f6d5ce54dc6f74",
+    "url": "/puzzle-react/static/js/main.be365a8d.chunk.js"
+  },
+  {
+    "revision": "9c7e2e92987cb32b2bfa",
+    "url": "/puzzle-react/static/js/runtime-main.b5821b7a.js"
+  },
+  {
+    "revision": "eed40435267cf1e8215f33f298d3f42c",
+    "url": "/puzzle-react/static/media/bg.eed40435.jpg"
+  }
+]);
